f4f86dc7e29ff06b40281f823af13dea734f7b91
HubertNastaly <h.nastaly@gmail.com>
2019-11-07 14:55:38 +0100

    All tasks

--- src/GameAdmin.jsx
+++ src/GameAdmin.jsx
@@ -5,32 +5,59 @@ class GameAdmin extends React.Component
 {
   constructor(props) {
     super(props);
-    this.state = { PlayerOneName: "", PlayerTwoName: "" }
-    this.changeNameEventHandler = this.changeNameEventHandler.bind(this);
+    this.state = { playerOneName: "", playerTwoName: "", playedTimesOne: 0, playedTimesTwo: 0, playerOneDisabled: 0}
+    this.playerOneContent = "Play";
+    this.playerTwoContent = "This user is playing now";
+    this.changeName = this.changeName.bind(this);
+    this.toggleButtons = this.toggleButtons.bind(this);
   }
-  changeNameEventHandler(event,player)
+  changeName(e,player)
   {
     if(player == 1)
     {
-      this.setState({PlayerOneName: event.target.value});
+      this.setState({playerOneName: e.target.value});
     }
     else
     {
-      this.setState({PlayerTwoName: event.target.value});
+      this.setState({playerTwoName: e.target.value});
     }
+  }
+  toggleButtons(event)
+  {
+      //swapping texts
+      this.playerOneContent = [this.playerTwoContent, this.playerTwoContent=this.playerOneContent][0];
+
+      //updating played times
+      if(this.state.playerOneDisabled)
+      {
+        this.setState((prevState, props) => ({playedTimesTwo: prevState.playedTimesTwo + 1}));
+      }
+      else
+      {
+        this.setState((prevState, props) => ({playedTimesOne: prevState.playedTimesOne + 1}));
+      }
+
+      //swapping disable attribute
+      this.setState((prevState,props) => ({playerOneDisabled: prevState.playerOneDisabled ^ 1}));
   }
   render()
   {
     return(
-      <div class="gameAdmin">
-        <Player playerNumber="One" playerName= {this.state.PlayerOneName} playedTimes="0"/>
-        <Player playerNumber="Two" playerName= {this.state.PlayerTwoName} playedTimes="0"/>
+      <div className="gameAdmin">
+        <Player playerNumber="One" playerName= {this.state.playerOneName} playedTimes={this.state.playedTimesOne} 
+        toggleButtons={this.toggleButtons} buttonContent={this.playerOneContent} buttonDisabled={this.state.playerOneDisabled}/>
+        <Player playerNumber="Two" playerName= {this.state.playerTwoName} playedTimes={this.state.playedTimesTwo}
+         toggleButtons={this.toggleButtons} buttonContent={this.playerTwoContent} buttonDisabled={!this.state.playerOneDisabled}/>
         <br></br>
-        <label>Set name of Player One</label> 
-        <input type="text" onChange={(e) => this.changeNameEventHandler(e,1)}></input>
+        <form>
+          <label>Set name of Player One</label> 
+          <input type="text" onChange={(e) => this.changeName(e,1)}></input>
+        </form>
         <br></br>
-        <label>Set name of Player Two</label> 
-        <input type="text" onChange={(e) => this.changeNameEventHandler(e,2)}></input>
+        <form>
+          <label>Set name of Player Two</label> 
+          <input type="text" onChange={(e) => this.changeName(e,2)}></input>
+        </form>
       </div>
     );
   }
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
 
-function Player(props)
+class Player extends React.Component
 {
-    return (
-      <div class="player">
+    constructor(props) {
+      super(props);
+    }
+    render()
+    {
+      return(
+      <div className="player">
         <div>
-          <h2>Player {props.playerNumber}</h2>
-          <h4>Name: {props.playerName}</h4>
-          <h4>Played number of times: {props.playedTimes}</h4>
+          <h2>Player {this.props.playerNumber}</h2>
+          <h4>Name: {this.props.playerName}</h4>
+          <h4>Played number of times: {this.props.playedTimes}</h4>
         </div>
-        <button onClick={(e) => props.toggleButtons(e)}>Player</button>
+        <button disabled={this.props.buttonDisabled} onClick={(e) =>{e.persist(); this.props.toggleButtons(e)} }>{this.props.buttonContent}</button>
       </div>
-    );
+      );
+    }
 }
 
 export default Player
